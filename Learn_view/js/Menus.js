fe581d7edd418bb0aca9eb1fffbd06a3accc9721
onion <antoniohenriquebv@gmail.com>
2023-06-16 17:28:51 -0300

    Revert "JavaScript"
    This reverts commit 6150cd2f180e4658c5d7b819ecfabab2e17fe730.

--- Learn_view/js/Menus.js
+++
@@ -1,5 +0,0 @@
-let carretDown = document.getElementsByClassName("carret-down")[0]
-carretDown.addEventListener("click",function(){
-    let filtros = document.getElementsByClassName("filtros")[0]
-    filtros.classList.toggle("altura")
-})
