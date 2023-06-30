4b15027918257240c165fc8f162e69ee63bf0211
onion <antoniohenriquebv@gmail.com>
2023-06-30 11:57:43 -0300

    Começo da Junção

--- Home/home-script.js
+++ Home/home-script.js
@@ -1,70 +1,51 @@
-function createNavLinks() {
-  const menuLinks = ["Home", "Perfil", "Login", "Cadastro", "Sugestão", "Filmes", "Séries"];
-  const navLinks = document.querySelector('.nav-links');
 
-  for (let index = 0; index < menuLinks.length; index++) {
-    let newListElement = document.createElement("li");
-    let newLink = document.createElement("a");
-    newLink.innerHTML = menuLinks[index];
-    newListElement.appendChild(newLink);
-    navLinks.appendChild(newListElement);
-  }
-}
-createNavLinks();
+const apiKey = '559a33d4fee9f2c2b010121bd11bc858';
 
 function capitalizeFirstLetter(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-async function fetchMovies(genre, moviesPerPage, apiKey) {
-  const genres_ids = {
-    guerra: 10752,
-    música: 10402,
-    história: 36
-  }
-  const currentGenreId = genres_ids[genre];
-  console.log(currentGenreId);
 
-  try {
-    const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${currentGenreId}&page=1&language=pt-BR&sort_by=popularity.desc&include_adult=false&vote_average.gte=10`);
-    const data = await response.json();
-    const filtermovies = data.results.filter(movie => movie.poster_path)
-    const movies = filtermovies.slice(0, moviesPerPage);
-    return movies;
-  } catch (error) {
-    console.error('Ocorreu um erro ao buscar os filmes:', error);
-    return [];
-  }
-}
+  const filmes = [
+    { id: "tt0995868", categoria: "Historia Esportes" },
+  { id: "tt5013056", categoria: "Historia" },
+  { id: "tt1016150", categoria: "Historia" },
+  { id: "tt0129290", categoria: "Medicina" },
+  { id: "tt0386792", categoria: "Medicina Historia" },
+  { id: "tt0137523", categoria: "Cinema" },
+  { id: "tt1186830", categoria: "Historia" },
+  { id: "tt1285016", categoria: "TI Historia" },
+  { id: "tt2084970", categoria: "TI Historia" },
+  { id: "tt2119532", categoria: "Historia" },
+  { id: "tt8404614", categoria: "Historia Politica" },
+  { id: "tt0914798", categoria: "Historia Politica" },
+  { id: "tt0857355", categoria: "Historia Politica" },
+  { id: "tt0318462", categoria: "Historia Politica Filosofia" },
+  { id: "tt0271383", categoria: "Cinema" },
+  ]
 
-function getMoviePoster(movie) {
-  return `https://image.tmdb.org/t/p/w220_and_h330_face/${movie.poster_path}`; 
-}
 
-async function renderPosters(listOfMovies, genre) {
-  const apiKey = '559a33d4fee9f2c2b010121bd11bc858';
-  const moviesPerPage = 5;
-  const movies = await fetchMovies(genre, moviesPerPage, apiKey) || [];
-  for (let index = 0; index < moviesPerPage; index ++) {
-    const listElement = document.createElement("li");
-    const posterImage = document.createElement("img");
-    posterImage.src = getMoviePoster(movies[index]);
-    posterImage.className = "poster-image"
-
-    listElement.appendChild(posterImage);
-    listOfMovies.appendChild(listElement)    
-  };
+async function renderPosters(id,listOfMovies){
+ let filme = await fetch(
+  `https://api.themoviedb.org/3/movie/${id}?language=pt-BR&api_key=2dc6e5babc6cd433199c68a7f1f81d40`
+).then(res=>res.json()).then(filme=>{
+ const listElement = document.createElement("li");
+ const posterImage = document.createElement("img");
+ posterImage.src =`https://image.tmdb.org/t/p/original/${filme.poster_path}`
+ posterImage.className = "poster-image"
+ listElement.appendChild(posterImage);
+ listOfMovies.appendChild(listElement)});
 }
 
 async function createMovieSections() {
   const mainTag = document.querySelector('main');
-  const genres = ['história', 'guerra', 'música'];
+  const genres = ['Historia', 'Medicina', 'Filosofia','TI','Esportes','Cinema','Politica'];
   const baseSubtitle = "Descubra filmes, documentários e séries sobre";
 
   for (let index = 0; index < genres.length; index ++) {
     const newSection = document.createElement("section");
     const newGenreDiv = document.createElement("div");
-    newGenreDiv.className = `${genres[index]}-title`;
+    newGenreDiv.className = `${genres[index]}-title title`;
 
     const genreTitle = document.createElement("h1");
     const genreSubtitle = document.createElement("h5");
@@ -81,8 +62,15 @@ async function createMovieSections() {
 
     newSection.appendChild(listOfMovies);
     mainTag.appendChild(newSection);
-
-    await renderPosters(listOfMovies, genres[index]);
+    for(j=0;j<filmes.length;j++){
+      console.log("categoria:",filmes[j].categoria)
+      console.log("genero:",genres[index])
+      console.log("bool:",filmes[j].categoria.includes(genres[index]))
+      if(filmes[j].categoria.includes(genres[index])){
+        await renderPosters(filmes[j].id,listOfMovies);
+      }
+    }
+    
   };
 };
 createMovieSections();
