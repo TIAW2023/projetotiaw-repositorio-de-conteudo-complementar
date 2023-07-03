e43e80655643ae61bb9563225b4b2a489418836a
onion <antoniohenriquebv@gmail.com>
2023-07-03 19:41:09 -0300

    att

--- Perfil/Tiaw Part 3/cardperfil.js
+++ Perfil/Tiaw Part 3/cardperfil.js
@@ -1,8 +1,10 @@
 const url = new URL(window.location);
+console.log(filmes)
 function openCard(posição) {
   url.searchParams.set("pos", posição);
   history.pushState({}, "", url);
   let parametros = new URLSearchParams(url.search);
+  if(filmes[posição].title){
   document.getElementById("cardbox").style.display = "flex";
   document.getElementById(
     "filme_img"
@@ -22,6 +24,28 @@ function openCard(posição) {
   barra.style.background = `conic-gradient(#21d07a ${
     Math.ceil(filmes[posição].vote_average * 10) * 3.6
   }deg,#204529 0deg)`;
+}
+else{
+  document.getElementById("cardbox").style.display = "flex";
+  document.getElementById(
+    "filme_img"
+  ).src = `https://image.tmdb.org/t/p/original/${filmes[posição].poster_path}`;
+  document.querySelector(
+    ".card span h1"
+  ).innerHTML = `${filmes[posição].name}`;
+  document.querySelector(".card span p").innerHTML = `${filmes[
+    posição
+  ].first_air_date.substring(0, 4)}`;
+  document.querySelector(
+    ".card span p:nth-child(3)"
+  ).innerHTML = `${filmes[posição].overview}`;
+  let barra = document.querySelector(".medidor"),
+    valorBarra = document.querySelector(".valor_barra");
+  valorBarra.textContent = `${Math.ceil(filmes[posição].vote_average * 10)}%`;
+  barra.style.background = `conic-gradient(#21d07a ${
+    Math.ceil(filmes[posição].vote_average * 10) * 3.6
+  }deg,#204529 0deg)`;
+}
   if(localStorage.getItem("favs")){
   if (localStorage.getItem("favs").includes(JSON.stringify(filmes[parametros.get("pos")]))) {
     bookmark.classList.add("fa-solid");
--- Perfil/Tiaw Part 3/perfilfilmes.js
+++ Perfil/Tiaw Part 3/perfilfilmes.js
@@ -3,12 +3,22 @@ let divFilmes = document.getElementById('filmes');
 let str = '';
 
 if(filmes){
+  
 for(let i=0;i<filmes.length;i++){
+  if(filmes[i].title){
   str+=`<div class="filme col-md-2 col-sm-5" onclick="openCard(${i})">
   <img src="https://image.tmdb.org/t/p/original/${filmes[i].poster_path}" class="img_filme">
   <h2 class="titulo_filme">${filmes[i].title}</h2>
 </div>`
 }
+else{
+  str+=`<div class="filme col-md-2 col-sm-5" onclick="openCard(${i})">
+  <img src="https://image.tmdb.org/t/p/original/${filmes[i].poster_path}" class="img_filme">
+  <h2 class="titulo_filme">${filmes[i].name}</h2>
+</div>`
+}
+}
+ 
 divFilmes.innerHTML = str;
 }
 else{
