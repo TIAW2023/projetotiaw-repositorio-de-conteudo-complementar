d40e76496ed29f3702d10b07950fcc2c8fab3156
onion <antoniohenriquebv@gmail.com>
2023-07-03 18:00:21 -0300

    Ajustes Perfil e Home

--- Perfil/Tiaw Part 3/perfil.js
+++ Perfil/Tiaw Part 3/perfil.js
@@ -1,6 +1,7 @@
 const inputFile = document.querySelector("#picture__input");
 const pictureImage = document.querySelector(".picture__image");
 const pictureImageTxt = "Escolher uma imagem";
+const img = document.createElement("img");
 let atual;
 let users = JSON.parse(localStorage.listaUser)
 for (let i = 0; i < users.length; i++) {
@@ -8,14 +9,20 @@ for (let i = 0; i < users.length; i++) {
     atual = i;
   }
 }
+function updateImg(){
+      img.classList.add("picture__img");
+      pictureImage.innerHTML = null;
+      pictureImage.appendChild(img);
+      img.src = users[atual].imagem
+}
+if(img.src==undefined){
 pictureImage.innerHTML = pictureImageTxt;
+}
 var username = document.getElementById("username");
 let nome = JSON.parse(localStorage.userLogado).nome;
 if(nome==undefined){
-  console.log("entrou")
   let teste = JSON.parse(localStorage.userLogado)
   teste.nome = teste.nomeCad
-  console.log(teste)
   localStorage.userLogado = JSON.stringify(teste)
 }
 username.innerHTML = JSON.parse(localStorage.userLogado).nome
@@ -27,8 +34,9 @@ inputFile.addEventListener("change", function (e) {
     const reader = new FileReader();
     reader.addEventListener("load", function (e) {
       const readerTarget = e.target;
-      const img = document.createElement("img");
       img.src = readerTarget.result;
+      users[atual].imagem = img.src;
+      localStorage.listaUser= JSON.stringify(users)
       img.classList.add("picture__img");
       pictureImage.innerHTML = "";
       pictureImage.appendChild(img);
@@ -70,6 +78,7 @@ function editName() {
   input.value = ''
 }
 }
+updateImg()
 
 
 // Simulando a criação do objeto userValid e salvando-o no localStorage
